ed07fb4bee440112f68acbafde1a46e6bd418358
Boris Borshevsky <bborshevsky@dropbox.com>
2020-04-10 10:52:35 +0300

    Updates

---
+++ precache-manifest.ce993649cfe957a23d607ed77d6980a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a3fe3234bffc6dde38d5d147160b74",
+    "url": "/coalition-game/index.html"
+  },
+  {
+    "revision": "ce38991c89aac47f8f7b",
+    "url": "/coalition-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a8b7cfaead6e70ffef46",
+    "url": "/coalition-game/static/js/2.8a355bf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coalition-game/static/js/2.8a355bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce38991c89aac47f8f7b",
+    "url": "/coalition-game/static/js/main.faf6cde9.chunk.js"
+  },
+  {
+    "revision": "dc6da7b642f1e7e04513",
+    "url": "/coalition-game/static/js/runtime-main.abd01dd6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coalition-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
